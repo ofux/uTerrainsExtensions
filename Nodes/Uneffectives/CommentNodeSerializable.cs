f644eddfdfa07f167c8e9d56acfa791fcdaea355
Olivier FUXET <olivier.fuxet@gmail.com>
2019-03-03 17:56:53 +0100

    Add comment node

---
+++ Nodes/Uneffectives/CommentNodeSerializable.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using UnityEngine;
+#if UNITY_EDITOR
+using UnityEditor;
+
+#endif
+
+namespace UltimateTerrains
+{
+    [PrettyTypeName("Comment")]
+    [Serializable]
+    public class CommentNodeSerializable : UneffectiveNodeSerializable
+    {
+        [SerializeField] private string comment;
+
+        public override string Title {
+            get { return "Comment"; }
+        }
+
+        public override int EditorWidth {
+            get { return 200; }
+        }
+
+        public override void OnEditorGUI(UltimateTerrain uTerrain)
+        {
+#if UNITY_EDITOR
+            comment = EditorGUILayout.TextArea(comment);
+#endif
+        }
+    }
+}
