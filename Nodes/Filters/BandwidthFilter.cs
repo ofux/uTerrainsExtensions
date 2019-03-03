bc81827d6b561a0be3c152025d9e1350cedaf1bb
Olivier FUXET <olivier.fuxet@gmail.com>
2019-03-03 20:59:53 +0100

    Add Bandwidth node

---
+++ Nodes/Filters/BandwidthFilter.cs
@@ -0,0 +1,18 @@
+﻿using UltimateTerrains;
+
+public class BandwidthFilter : FilterNode
+{
+    private readonly double min;
+    private readonly double max;
+
+    public BandwidthFilter(CallableNode input, CallableNode maskInput, double intensity, double min, double max) : base(input, maskInput, intensity)
+    {
+        this.min = min;
+        this.max = max;
+    }
+
+    protected override double ExecuteFilter(double x, double y, double z, CallableNode flow, double inputValue)
+    {
+        return min < inputValue && inputValue < max ? 1 : 0;
+    }
+}
--- Nodes/Filters/BandwidthFilterSerializable.cs
+++ Nodes/Filters/BandwidthFilterSerializable.cs
@@ -9,22 +9,22 @@ using UnityEditor;
 #endif
 
 
-[PrettyTypeName("Clamp")]
+[PrettyTypeName("Bandwidth")]
 [Serializable]
-public class ClampFilterSerializable : FilterNodeSerializable
+public class BandwidthFilterSerializable : FilterNodeSerializable
 {
     public override string Title {
-        get { return "Clamp"; }
+        get { return "Bandwidth"; }
     }
 
-    [SerializeField] private Vector2 outBounds = new Vector2(-1,1);
+    [SerializeField] private Vector2 bandwidth = new Vector2(-1,1);
 
     public override void OnEditorGUI(UltimateTerrain uTerrain)
     {
 #if UNITY_EDITOR
         EditorGUIUtility.labelWidth = 60;
-        EditorUtils.CenteredBoxedLabelField(string.Format("out ∈ [{0},{1}]", outBounds.x, outBounds.y));
-        outBounds = EditorUtils.MinMaxField(outBounds);
+        EditorUtils.CenteredBoxedLabelField(string.Format("out=1 if in∈[{0},{1}]\nout=0 otherwise", bandwidth.x, bandwidth.y));
+        bandwidth = EditorUtils.MinMaxField(bandwidth);
         base.OnEditorGUI(uTerrain);
         EditorGUIUtility.labelWidth = 0;
 #endif
@@ -32,6 +32,6 @@ public class ClampFilterSerializable : FilterNodeSerializable
 
     public override IGeneratorNode CreateModule(UltimateTerrain uTerrain, List<CallableNode> inputs)
     {
-        return new ClampFilter(inputs[0], inputs[1], Intensity, outBounds.x, outBounds.y);
+        return new BandwidthFilter(inputs[0], inputs[1], Intensity, bandwidth.x, bandwidth.y);
     }
 }
