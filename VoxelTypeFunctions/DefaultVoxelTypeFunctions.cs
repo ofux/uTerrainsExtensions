56e0d0c986f5609a072ff5d73efae7620fadecac
Olivier FUXET <olivier.fuxet@gmail.com>
2019-03-29 13:27:05 +0100

    Add new PBR shader

--- VoxelTypeFunctions/DefaultVoxelTypeFunctions.cs
+++ VoxelTypeFunctions/DefaultVoxelTypeFunctions.cs
@@ -10,18 +10,24 @@ namespace UltimateTerrains
     [Serializable]
     public class DefaultVoxelTypeFunctions : AbstractVoxelTypeFunctions
     {
-        [SerializeField] private Color32
-            vertexColor = Color.clear;
+        [SerializeField] private Color32 vertexColor = Color.red;
+        [SerializeField] private Vector4 uv2 = Vector4.zero;
 
         public override Color32 GetVertexColor(Vector3d meshWorldPosition, Vector3 vertexPosition, Vector3 vertexNormal)
         {
             return vertexColor;
         }
 
+        public override Vector4 GetVertexUVW2(Vector3d meshWorldPosition, Vector3 vertexPosition, Vector3 vertexNormal)
+        {
+            return uv2;
+        }
+
         public override void OnEditorGUI(UltimateTerrain uTerrain, VoxelType voxelType)
         {
 #if UNITY_EDITOR
             vertexColor = EditorGUILayout.ColorField("Vertex Color:", vertexColor);
+            uv2 = EditorGUILayout.ColorField("UV2 (X = R, Y = G):", uv2);
 #endif
         }
     }
